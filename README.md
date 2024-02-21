00df22332c40c0cfb81448dec5c98754077664d2
Udo Thiel <udo@udo-thiel.de>
2024-02-21 20:42:52 +0100

    integrated CLI functionality into UI executable

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ Temperatures are averaged over the selected sensors.
 
 This results in a typical CPU load of less then 0.2%.
 
+## Prerequisites
+- macOS 13.0 or later
+- Apple Silicon Mac 
+
 ## Installation
 ### Install binary
 The release includes a binary that is notarised by Apple. It has to be placed in the `/Applications` folder.
@@ -22,3 +26,9 @@ Use Github's `Open in Xcode` feature to fetch one of the current branches, or do
 There are no dependencies, therefore compilation should be straightforward. 
 Be sure to sign the binaries correctly, 
 either using your own team, or sign to run locally. 
+
+## Command-line Interface
+If the executable is called with the `-dump` option, it will dump the current temperature values to `stdout`. 
+Any value after this option will be interpreted as a time interval in seconds to repeat the dump.
+
+Example: `/Applications/TinyTemp.app/Contents/MacOS/TinyTemp -dump 1` will dump the current temperature values to `stdout` every second.
--- TinyTemp/main.m
+++ TinyTemp/main.m
@@ -6,10 +6,57 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "../TinyTemp/IOHID/IOHID.h"
+
+static void timerCallback(CFRunLoopTimerRef timer, void *info) {
+	IOHID *iohid = (__bridge IOHID *) info;
+	puts([NSDate.date descriptionWithLocale:NSLocale.currentLocale].UTF8String);
+	puts(iohid.description.UTF8String);
+}
+
+
 
 int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // Setup code that might create autoreleased objects goes here.
+
+	NSArray* arguments = NSProcessInfo.processInfo.arguments;
+
+	if ([arguments containsObject:@"-dump"]) {
+		// enter CLI mode and handle arg(s)
+		
+		dup2(fileno(stdout), fileno(stderr));// redirect stderr (NSLog) to stdout
+		
+		IOHID *iohid		= IOHID.shared;
+		BOOL repeat			= NO;
+		NSInteger interval	= 0;
+		CFRunLoopTimerRef timer;
+		
+		if (arguments.count > 2) {
+			repeat		= YES;
+			interval	= [arguments[2] integerValue];
+		}
+		
+		if (repeat) {
+			CFRunLoopTimerContext timerContext;
+			timerContext.version = 0;
+			timerContext.info = (__bridge void *)(iohid);
+			timerContext.retain = NULL;
+			timerContext.release = NULL;
+			timerContext.copyDescription = NULL;
+			
+			timer	= CFRunLoopTimerCreate(kCFAllocatorDefault,
+										   CFAbsoluteTimeGetCurrent(),
+										   interval, 0, 0, timerCallback, &timerContext);
+			
+			CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, kCFRunLoopDefaultMode);
+			CFRunLoopRun();
+		} else {
+			puts(iohid.description.UTF8String);
+		}
+		return 0;
+	} else {
+		// switch to UI mode
+		ProcessSerialNumber psn = { 0, kCurrentProcess };
+		TransformProcessType(&psn, kProcessTransformToForegroundApplication);
+		return NSApplicationMain(argc, argv);
 	}
-	return NSApplicationMain(argc, argv);
 }
